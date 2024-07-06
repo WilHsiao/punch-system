7fcdb46aa9358762cf9ff4e2c9c8298b6532fc0c
WilHsiao <give1692000@gmail.com>
2024-07-06 12:22:30 +0800

    Update page.js

--- src/app/punch/page.js
+++ src/app/punch/page.js
@@ -29,7 +29,7 @@ export default function Punch() {
 
     const handleScan = async (data) => {
         const now = Date.now();
-        if (data && now - lastScanTime.current > 2000 && !isProcessing.current) {
+        if (data && now - lastScanTime.current > 1500 && !isProcessing.current) {
             isProcessing.current = true;
             console.log("QR Code detected: ", data.text);
             setUid(data.text);
@@ -38,13 +38,15 @@ export default function Punch() {
             lastScanTime.current = now;
             setTimeout(() => {
                 isProcessing.current = false;
-            }, 2000);
+            }, 1500);
         }
     };
 
     const handleError = (err) => {
         console.error("QR Reader Error: ", err);
-        toast.error(`掃描 QR Code 失敗: ${err}`);
+        toast.error(`掃描 QR Code 失敗: ${err}`, {
+            autoClose: 2000,
+        });
     };
 
     const handlePunch = async (uid) => {
@@ -59,38 +61,46 @@ export default function Punch() {
                 const punchRef = ref(database, `users/${uid}/punches/${new Date().toISOString().replace(/\W/g, '')}`);
                 await set(punchRef, punchData);
                 console.log("Punch recorded successfully.");
-                toast.success(`${userData.name} 打卡成功！`);
+                toast.success(`${userData.name} 打卡成功！`, {
+                    autoClose:2000,
+                });
             } else {
-                toast.error('用戶不存在！');
+                toast.error('用戶不存在！', {
+                    autoClose:2000,
+                });
             }
 
             setButtonDisabled(false);
             setTimeout(() => {
                 setScanning(true);
-            }, 3000);
+            }, 2000);
 
         } catch (error) {
             console.error("Error handling punch: ", error);
-            toast.error('打卡過程出現錯誤');
+            toast.error('打卡過程出現錯誤', {
+                autoClose:2000,
+            });
         } finally {
             setButtonDisabled(false);
             setTimeout(() => {
                 setScanning(true);
-            }, 3000);
+            }, 2000);
         }
     };
 
     return (
         <>
             <Navigation />
-            <div>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
                 {scanning && (
-                    <QrReader
-                        delay={300}
-                        onError={handleError}
-                        onScan={handleScan}
-                        style={{ width: '100%' }}
-                    />
+                    <div style={{ width: '50%', height: '50%' }}>
+                        <QrReader
+                            delay={300}
+                            onError={handleError}
+                            onScan={handleScan}
+                            style={{ width: '100%', height: '100%' }}
+                        />
+                    </div>
                 )}
             </div>
             <ToastContainer />
