21be94e728fd6858f8fc10327781e9cc17a895e9
WilHsiao <give1692000@gmail.com>
2024-07-08 22:11:51 +0800

    update user-login-process

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
           <h1>打卡系統 v.2</h1>
-          <p>我是首頁我是首頁!</p>
+          <p>我是首頁!</p>
         </div>
       </main>
     </>
