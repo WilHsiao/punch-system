31b606568afea3d0912a872f7fb3ad16dd8aa8dc
WilHsiao <give1692000@gmail.com>
2024-07-17 14:27:27 +0800

    update

--- src/components/punch-system-nav.js
+++ src/components/punch-system-nav.js
@@ -8,7 +8,7 @@ export default function PunchSystemNav(){
   return (
     <>
       <nav className="w-full bg-gray-600 py-4">
-        <div className="flex items-center justify-center space-x-8">
+        <div className="flex items-center justify-center space-x-4">
           <Link href="/punch-system/punch-v2" legacyBehavior>
             <a className="text-lg font-bold bg-blue-300 text-black px-4 py-2 rounded hover:bg-blue-400 transition duration-300">
               一般打卡
