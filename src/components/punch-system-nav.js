318da15e7691e018d38ef89d9e26bb8221ced75b
WilHsiao <give1692000@gmail.com>
2024-07-28 18:17:44 +0800

    Update punch-system-nav.js

--- src/components/punch-system-nav.js
+++ src/components/punch-system-nav.js
@@ -11,17 +11,22 @@ export default function PunchSystemNav(){
         <div className="flex items-center justify-center space-x-4">
           <Link href="/punch-system/punch-v2" legacyBehavior>
             <a className="text-lg font-bold bg-blue-300 text-black px-4 py-2 rounded hover:bg-blue-400 transition duration-300">
-              一般打卡
+              一般打卡【主管】
             </a>
           </Link>
           <Link href="/punch-system/punch-manual" legacyBehavior>
             <a className="text-lg font-bold bg-blue-300 text-black px-4 py-2 rounded hover:bg-blue-400 transition duration-300">
-              補打卡
+              補打卡【主管】
             </a>
           </Link>
           <Link href="/punch-system/query-punch-data" legacyBehavior>
             <a className="text-lg font-bold bg-blue-300 text-black px-4 py-2 rounded hover:bg-blue-400 transition duration-300">
-              查詢打卡資料
+              查詢資料【主管】
+            </a>
+          </Link>
+          <Link href="/punch-system/query-self-data" legacyBehavior>
+            <a className="text-lg font-bold bg-blue-300 text-black px-4 py-2 rounded hover:bg-blue-400 transition duration-300">
+              我的打卡紀錄
             </a>
           </Link>
         </div>
