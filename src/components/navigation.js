38bb1cbbf297ca1462e6b829fe34963f605fefb1
WilHsiao <give1692000@gmail.com>
2024-08-26 18:37:47 +0800

    update navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -117,7 +117,7 @@ export default function Navigation() {
     <>
       {isMobile && (
         <button onClick={toggleMenu} className="fixed top-4 left-4 z-50 text-white bg-blue-500 p-2 rounded">
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <svg className="w-6 h-6 z-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
           </svg>
         </button>
