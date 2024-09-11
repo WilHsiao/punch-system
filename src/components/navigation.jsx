fdf95161aa19a322c67dc7f423453c64b63b17e8
TongErLai <tony@procrustes.info>
2024-09-11 09:05:51 +0800

    test

---
+++ src/components/navigation.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const navigation = () =>{
+	return	(
+	<>
+		<div className='fixed w-20 h-screen p-4 bg-white border-r-[3px] border-blue-600 flex flex-col justify-between'>
+			
+
+		</div>
+		<div className='flex justify-between ml-20 p-4 '>
+			<h2>Dashboard</h2>
+			<h2>Dashboard</h2>
+		</div>
+	</>
+	)
+
+}
+
+export default navigation
